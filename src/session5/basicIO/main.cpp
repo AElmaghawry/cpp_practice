e4a614fa4970210fea891f8405110b5dbddd98f9
AElmaghawry <AElmaghawry>
2023-02-14 23:52:34 +0400

    basic IO

--- src/session5/basicIO/main.cpp
+++ src/session5/basicIO/main.cpp
@@ -3,5 +3,35 @@
 using namespace std ; 
 
 int main (){
-    cout << " Hello world !"
+    // cout << " Hello world !" << endl ; 
+    // cout << "Hello" ; 
+    // cout << "world" <<endl ; 
+    
+    // cout << "Hello world" << endl ; 
+    // cout << "Helloo" << " world!"<< endl ; 
+    // cout << "Hello" << "world!\n" ;
+    // cout << "Hello\nOut\nThere\n";
+
+    int num1 ; 
+    int num2 ; 
+    double num3 ; 
+
+    // cout << " Enter an Integer !" ; 
+    // cin >> num1 ; 
+    // cout << " you entered: " << num1 <<endl ; 
+    
+    // cout << "Enter the first int " ; 
+    // cin >> num1 ; 
+    // cout << " enter the seconde int" ; 
+    // cin >> num2 ; 
+    // cout << " The first int " << num1 << "The 2nd inte" << num2 ; 
+
+    // cout << "Enter the first int and 2nd int" ; 
+    // cin >> num1 >> num2 ; 
+    // cout << " The first int " << num1 << "The 2nd int" << num2 ; 
+
+    
+
+    return 0 ; 
+    
 }
