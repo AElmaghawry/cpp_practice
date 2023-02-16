3c8ac2992d286f5f76cc82d311b62f09ce292a73
AElmaghawry <AElmaghawry>
2023-02-16 11:38:14 +0400

    Peremitive types

---
+++ src/section6/builtInTypes/main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std ; 
+
+int main () 
+{
+    char  middleName {'J'}; 
+    cout << "My middle name is "<< middleName << endl ;
+
+    unsigned short int examScore {50}; 
+    cout << "My Exam score is " << examScore << endl; 
+
+    int countiresRepresented {65} ; 
+    cout << countiresRepresented << endl ; 
+
+    return 0; 
+
+
+
+}
