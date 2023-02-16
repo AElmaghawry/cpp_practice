02615706ee769df6f9fe29fd8a30cd15e7aa7fbc
AElmaghawry <AElmaghawry>
2023-02-16 11:17:38 +0400

    diff. between Global and local var.

---
+++ src/section6/globalVariables/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std ;
+
+
+int age{19} ;  // it is not a good practice to use gloabl varibales 
+
+int main ()
+{   
+    int age {16} ; 
+    cout << age << endl; 
+
+
+    return 0; 
+}
